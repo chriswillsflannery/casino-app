8d527366ad680fbbe2b18fbd4623f9b80dcf6c0d
Chris <cflannery@williamhill.us>
2022-01-24 15:49:32 -0500

    add assets

---
+++ src/gamesConfig.js
@@ -0,0 +1,22 @@
+export const CASINO_GAMES = [
+  {
+    title: "Asgardian Stones",
+    category: "Slots",
+    thumbnail: "asgardianstones.jpg"
+  },
+  {
+    title: "Blackjack",
+    category: "Table Games",
+    thumbnail: "blackjack.jpg"
+  },
+  {
+    title: "Star Burst",
+    category: "Slots",
+    thumbnail: "starburst.jpg"
+  },
+  {
+    title: "Wild Worlds",
+    category: "Slots",
+    thumbnail: "wildworlds.jpg"
+  }
+];
